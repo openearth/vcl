77ca01a2fcab45edf8463206b383cbbf9d0630f1
Guus <guus.vanhemert@deltares.nl>
2023-06-22 14:35:50 +0200

    Small changes and loading new scenario data

--- vcl/load_data.py
+++ vcl/load_data.py
@@ -12,6 +12,7 @@ def load():
     # Dataset for ground water model (concentrations)
     # Dataset ds is ordered with (z,y,x) coordinates
     ds = xr.open_dataset(data_dir.joinpath("concentratie_data_gw_model.nc"))
+    ds_n = xr.open_dataset(data_dir.joinpath("conc_nieuw.nc"))
 
     # Dataset of bathymetry
     ds_b0 = rxr.open_rasterio(data_dir.joinpath("originele_bodem.tif"))
@@ -20,4 +21,4 @@ def load():
     # Read satellite image with surrounding sea
     sat = mpimg.imread(data_dir.joinpath("terschelling-sat2.png"))
 
-    return {"ds": ds, "ds_b0": ds_b0, "sat": sat}
+    return {"ds": ds, "ds_n": ds_n, "ds_b0": ds_b0, "ds_b0_n": ds_b0_n, "sat": sat}
--- vcl/prep_data.py
+++ vcl/prep_data.py
@@ -14,12 +14,18 @@ import vcl.load_data
 
 def preprocess(datasets):
     ds = datasets["ds"]
+    ds_n = datasets["ds_n"]
     ds_b0 = datasets["ds_b0"]
+    ds_b0_n = datasets["ds_b0_n"]
     sat = datasets["sat"]
     # Replace negative concentrations (due to model errors) with 0
     ds = ds.where((ds.conc >= 0) | ds.isnull(), other=0)
+    ds_n = ds_n.where((ds_n.conc >= 0) | ds_n.isnull(), other = 0)
+
     bodem0 = np.array(ds_b0[0, 1435:2466, 1201:2970])
     bodem0[np.where(bodem0 == -9999)] = -43.8
+    bodem0_n = np.array(ds_b0_n[0,1435:2466,1201:2970])
+    bodem0_n[np.where(bodem0_n == -9999)] = -43.8
 
     # Scale satellite image to bathymetry shapes
     sat_scaled = cv2.resize(
@@ -38,10 +44,13 @@ def preprocess(datasets):
 
     # Each z layer of concentration dataset needs to be rotated and cropped seperately to account for the slices in x and y direction
     # Create dummy array of one layer and apply the function to it to obtain its shape
+    # Note that the first dimension of ds_n is time instead of z
     dummy = vcl.data.rotate_and_crop(ds.conc.values[0, :, :], -15)
     rot_ds = np.zeros((ds.conc.shape[0], dummy.shape[0], dummy.shape[1]))
+    rot_ds_n = np.zeros((ds_n.conc.shape[1], dummy.shape[0], dummy.shape[1]))
     for i in range(rot_ds.shape[0]):
         rot_ds[i, :, :] = vcl.data.rotate_and_crop(ds.conc.values[i, :, :], -15)
+        rot_ds_n[i, :, :] = vcl.data.rotate_and_crop(ds_n.conc.values[0, i, :, :], -15)
 
     rot_img_shade = vcl.data.rotate_and_crop(img_shade, -15)
     rot_bodem0 = vcl.data.rotate_and_crop(bodem0, -15)
@@ -65,23 +74,19 @@ def preprocess(datasets):
     nbpixels_x = 160
     nbpixels_y = rot_img_shade.shape[0]
 
-    conc_contours_x = np.zeros((nbpixels_x, nbpixels_y, rot_ds.shape[-1])) - 2
-    for i in range(conc_contours_x.shape[-1]):
-        cf = plt.contourf(Y1, Z1, rot_ds[:, :, i], levels=[0, 1.5, 16])
-        conc_contours_x[:, :, i] = np.flip(
-            vcl.data.contourf_to_array(cf, nbpixels_x, nbpixels_y, Y1, Z1), axis=0
-        )
-        # Change all values smaller than -1 to nan, since they were nan values before converting the contours to arrays
-        conc_contours_x[:, :, i][np.where(conc_contours_x[:, :, i] < -1)] = np.nan
-    plt.close("all")
+    conc_contours_x = vcl.data.contourf_to_array_3d(rot_ds, nbpixels_x, nbpixels_y, Y1, Z1, levels=[0, 1.5, 16])
+    conc_contours_x_n = vcl.data.contourf_to_array_3d(rot_ds_n, nbpixels_x, nbpixels_y, Y1, Z1, levels=[0, 1.5, 16])
+
     updated_datasets = {
         "nbpixels_x": nbpixels_x,
         "nbpixels_y": nbpixels_y,
         "conc_contours_x": conc_contours_x,
+        "conc_contours_x_n": conc_contours_x_n,
         "sat": rot_img_shade,
         "extent_n": extent_n,
         "X2": X2,
         "Y2": Y2,
         "conc": rot_ds,
+        "conc_n": rot_ds_n,
     }
     return updated_datasets
